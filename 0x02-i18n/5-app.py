2c7407c14ddaf2c17c1185cfce58b3abba495f18
otega07 <okiriotega@gmail.com>
2024-09-02 09:30:05 +0100

    task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -48,13 +48,13 @@ def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
     if g.user:
-        locale = g.user.get('locale', '')
-        if locale in app.config["LANGUAGES"]:
-            return locale
+        user_locale = g.user.get('locale', '')
+        if user_locale in app.config["LANGUAGES"]:
+            return user_locale
     else:
-        locale = request.args.get('locale')
-        if locale and locale in app.config['LANGUAGES']
-            return locale
+        user_locale = request.args.get('locale')
+        if user_locale and user_locale in app.config['LANGUAGES']
+            return user_locale
 
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
